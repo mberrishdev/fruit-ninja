15c926297b3b41c60ca09fbd0525c2202b7a126d
Mikheil Berishvili <mikheil.berishvili12@gmail.com>
2025-08-08 15:05:30 +0400

    Enhance CORS configuration in backend and update frontend HTML files with improved meta tags for SEO and social sharing. Add loading animation to index.html and unify game title across all pages.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: [
+      'https://fruitninja.mberrishdev.me',
+      'http://localhost:3000',  // For local development
+      'http://localhost:5000'   // For local development
+    ],
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+  });
+
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
